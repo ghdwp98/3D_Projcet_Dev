8e167f09db2ad5aba5f3f152f501f01511af3112
재훈 정 <ghdwp98@naver.com>
2024-04-16 01:36:20 +0900

    2맵 npc의 위치 및 대화 순서 destination 임시 완성 -->npc 애니메이션 및 mesh 넣어줘야함.

--- Assets/JeongJH/Script/Objects/Dialog/DialogTest.cs
+++ Assets/JeongJH/Script/Objects/Dialog/DialogTest.cs
@@ -21,9 +21,6 @@ public class DialogTest : MonoBehaviour
 
 		//npc의 위치에 따라 출력되는 system의 순서를 제어해 줘야함 (목표치에 도착하면 다음거 출력되도록 )
 
-
-
-
 		 //대사 분기 사이에 원하는 행동을 추가할 수 있다.
 		 //캐릭터를 움직이거나 아이템을 획득하는 등의.. 현재는 5-4-3-2-1 카운트 다운 실행
 		/*textCountdown.gameObject.SetActive(true);
@@ -39,7 +36,7 @@ public class DialogTest : MonoBehaviour
 		textCountdown.gameObject.SetActive(false);*/
 
 		// 두 번째 대사 분기 시작
-		yield return new WaitUntil(()=>dialogSystem02.UpdateDialog());
+		//yield return new WaitUntil(()=>dialogSystem02.UpdateDialog());
 
 		/*textCountdown.gameObject.SetActive(true);
 		textCountdown.text = "The End";*/
