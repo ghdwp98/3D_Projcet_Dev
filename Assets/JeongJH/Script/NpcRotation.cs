a098654c002590139dcab84463ef2d88862f145c
ghdwp98 <ghdwp98@naver.com>
2024-04-16 21:16:58 +0900

    end end 외곽선+ 꼬마

---
+++ Assets/JeongJH/Script/NpcRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcRotation : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+        
+    }
+}
