9102c6781103afc8f65dcedae63a61ac5b86eeb3
YJ <cvcv_cv@naver.com>
2024-04-16 21:08:15 +0900

    feat : 오류 수정 (4) - 엔딩씬 대화 다 보면 메인씬으로 이동

--- Assets/JeongJH/Script/Scenes/EndingScene.cs
+++ Assets/JeongJH/Script/Scenes/EndingScene.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class EndingScene : BaseScene
 {
+    float time;
     // ¿£µù ¾À ÀüÈ¯. 
     public override IEnumerator LoadingRoutine()
     {
         yield return null; 
     }
+
+	public void Update()
+	{
+		if(Time.timeScale != 0)
+        {
+            time += Time.deltaTime;
+            if(time > 5f)
+            {
+                Manager.Scene.LoadScene("MainScene");            }
+        }
+	}
+
+
 }
